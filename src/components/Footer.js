5d2db15bf4f278245b5f66f2cd58990f0823f25c
htunyarzar80 <htunyarzar2001@gmail.com>
2024-07-21 22:06:57 +0630

    [Complete] footer

---
+++ src/components/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <span className="footer-text">
+        Created with ❤️ by {" "}
+        <a href="https://github.com/tunyarzartoe-coding" target="_blank" rel="noopener noreferrer" className="footer-link">
+         Tun Yar Zar Toe
+        </a>
+      </span>
+    </div>
+  )
+}
+
+export default Footer
--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
+import Footer from './components/Footer';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <Footer/>
   </React.StrictMode>
 );
